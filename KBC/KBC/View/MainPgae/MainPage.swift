214e064d6bdee3d127258f76474023044bf9872b
kimsor1 <imoiksr@gmail.com>
2024-06-05 15:32:41 +0900

    SwiftUI 프로젝트 생성

---
+++ KBC/KBC/View/MainPgae/MainPage.swift
@@ -0,0 +1,18 @@
+//
+//  MainPage.swift
+//  KBC
+//
+//  Created by 김소리 on 6/5/24.
+//
+
+import SwiftUI
+
+struct MainPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainPage()
+}
