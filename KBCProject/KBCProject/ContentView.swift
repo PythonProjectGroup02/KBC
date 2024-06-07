06f7863ad5f4bc60605047dc12971acb1cd71a2a
kimsor1 <imoiksr@gmail.com>
2024-06-07 17:33:41 +0900

    swift 기본 구성

--- KBCProject/KBCProject/ContentView.swift
+++ KBCProject/KBCProject/ContentView.swift
@@ -8,16 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var selectPage = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-            Text("ssibal")
-                .font(.custom("KBO_bold", size: 30))
-        }
-        .padding()
+        HStack(content: {
+            Text("KBC")
+                .font(.system(size: 30))
+                .bold()
+                .foregroundStyle(Color.white)
+                .frame(width: 1000,height: 60)
+        })
+        .background(Color(red: 0.057, green: 0.139, blue: 0.328))
+        
+        TabView(selection: $selectPage,
+                content:  {
+            MainPage()
+                .tabItem {
+                    Image(systemName: "house.fill")
+                }
+            ScorePage()
+                .tabItem {
+                    Image(systemName: "flag.2.crossed.fill")
+                }
+            SchedulePage()
+                .tabItem {
+                    Image(systemName: "calendar")
+                }
+            MapPage()
+                .tabItem {
+                    Image(systemName: "map.fill")
+                }
+            MyPage()
+                .tabItem {
+                    Image(systemName: "person.fill")
+                }
+        })
     }
 }
 
